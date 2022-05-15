f0bbb0777368f919a06e3292d3841602ea01b946
arbaz <54329740+arbazbagwan@users.noreply.github.com>
2022-05-15 21:32:21 +0530

    Update App.js

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ const app = express();
 app.get('/', (req, res) => {
  
     // Sending the response
-    res.send('Hello World!')
+    res.send('Hello World!!')
     
     // Ending the response
     res.end()
